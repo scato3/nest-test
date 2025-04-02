17b43522d8fd159a8e5b1cc898198a9a0d8c3b98
scato3 <tlsgustn93@naver.com>
2025-04-02 14:23:46 +0900

    불필요한 파일 삭제

--- src/auth/dto/login.dto.ts
+++
@@ -1,3 +0,0 @@
-import { LoginDto } from '../../types';
-
-export { LoginDto };
--- src/types/mysql2.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'mysql2/promise' {
-  export interface Pool {
-    query(sql: string, values?: any[]): Promise<any>;
-  }
-
-  export function createPool(config: any): Pool;
-}
